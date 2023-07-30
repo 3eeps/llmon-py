fae4954609ed282bd231fee635b0d92563115c29
3eeps <55860052+3eeps@users.noreply.github.com>
2023-07-30 18:28:07 -0400

    Update frontend.py

--- frontend.py
+++ frontend.py
@@ -4,16 +4,12 @@ from langchain.memory import ConversationBufferMemory
 from langchain.callbacks.manager import CallbackManager
 from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
 
-# TODO - get tokens per message, when model reaches limit, summerize with summary buffer and add back to model memory
-# TODO - add support for multiple backends, switch models, options to change settings
-# MAYBE - irc style chat with multiple bots in a room? seems fun heh
-
 def run_model(model, mem_buffer):       
     user_input = input(Fore.BLUE + "> ")
     llm = ConversationChain(llm=model, memory=mem_buffer, verbose=False)
-    
+
     print (Fore.RED)
-    llm.predict(input = user_input)
+    llm.predict(input=user_input)
     print("\n")
 
 def main():
