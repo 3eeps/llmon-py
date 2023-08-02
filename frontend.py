fb8aeaa672480f1634e62d5dd2b374fcabdc9d50
3eeps <55860052+3eeps@users.noreply.github.com>
2023-08-01 21:30:32 -0400

    simplified

--- frontend.py
+++ frontend.py
@@ -1,26 +1,22 @@
+# /codespace/llm-frontend.py
 from colorama import Fore
 from langchain import LlamaCpp, ConversationChain
-from langchain.memory import ConversationBufferMemory
+from langchain.memory import ConversationSummaryBufferMemory
 from langchain.callbacks.manager import CallbackManager
 from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
 
-def run_model(model, mem_buffer):       
-    user_input = input(Fore.BLUE + "> ")
-    llm = ConversationChain(llm=model, memory=mem_buffer, verbose=False)
-
-    print (Fore.RED)
-    llm.predict(input=user_input)
-    print("\n")
-
-def main(ctx_size, threads):
+def main():
     callback_manager = CallbackManager([StreamingStdOutCallbackHandler()])
-    mem_buffer = ConversationBufferMemory(ai_prefix="Johnny 5", human_prefix="3eeps")
-    model = LlamaCpp(model_path="./based-30b.ggmlv3.q4_K_M.bin", n_ctx=ctx_size, n_threads=threads, callback_manager=callback_manager, verbose=False)
+    model = LlamaCpp(model_path="./based-30b.ggmlv3.q4_K_M.bin", n_ctx=2048, n_threads=10, callback_manager=callback_manager, verbose=False)
+    llm = ConversationChain(llm=model, memory=ConversationSummaryBufferMemory(ai_prefix="Johnny 5", llm=model, max_token_limit=2048, verbose=False))
     
     print (Fore.YELLOW + "llmon-py")
     user_input = None
     while user_input != "exit":
-        run_model(model, mem_buffer)
+        user_input = input(Fore.BLUE + "> ")
+        print (Fore.RED)
+        llm.predict(input=user_input)
+        print("\n")
 
 if __name__ == "__main__":
-    main(2048, 10)
+    main()
