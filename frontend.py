5525dba76546c0accb98a7e1b5a917fb30985f96
3eeps <55860052+3eeps@users.noreply.github.com>
2023-08-01 23:37:57 -0400

    small changes

--- frontend.py
+++ frontend.py
@@ -5,9 +5,9 @@ from langchain.memory import ConversationSummaryBufferMemory
 from langchain.callbacks.manager import CallbackManager
 from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
 
-def main(ctx_size, threads, ai_name):
+def main(model, ctx_size, threads, ai_name):
     callback_manager = CallbackManager([StreamingStdOutCallbackHandler()])
-    model = LlamaCpp(model_path="based-30b.ggmlv3.q4_K_M.bin", n_ctx=ctx_size, n_threads=threads, callback_manager=callback_manager, verbose=False)
+    model = LlamaCpp(model_path=model, n_ctx=ctx_size, n_threads=threads, callback_manager=callback_manager, verbose=False)
     llm = ConversationChain(llm=model, memory=ConversationSummaryBufferMemory(ai_prefix=ai_name, llm=model, max_token_limit=2048, verbose=False))
     
     print (Fore.YELLOW + "llmon-py")
@@ -19,4 +19,4 @@ def main(ctx_size, threads, ai_name):
         print("\n")
 
 if __name__ == "__main__":
-    main(2048, 10, "Johnny 5")
+    main("based-30b.ggmlv3.q4_K_M.bin", 2048, 10, "Johnny 5")
