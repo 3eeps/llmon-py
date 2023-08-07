0aedad22d064972c2908eb74ab1e18d609e11208
3eeps <55860052+3eeps@users.noreply.github.com>
2023-08-07 11:37:23 -0400

    added chat logging

--- frontend.py
+++ frontend.py
@@ -1,22 +1,31 @@
 # /codespace/llm-frontend.py
+import logging
 from colorama import Fore
 from langchain import LlamaCpp, ConversationChain
-from langchain.memory import ConversationSummaryBufferMemory
+from langchain.memory import ConversationBufferMemory
 from langchain.callbacks.manager import CallbackManager
 from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
 
-def main(model, ctx_size, threads, ai_name):
-    callback_manager = CallbackManager([StreamingStdOutCallbackHandler()])
-    model = LlamaCpp(model_path=model, n_ctx=ctx_size, n_threads=threads, callback_manager=callback_manager, verbose=False)
-    llm = ConversationChain(llm=model, memory=ConversationSummaryBufferMemory(ai_prefix=ai_name, llm=model, max_token_limit=2048, verbose=False))
-    
+def main(model, ctx_size, cores, ai_name, user_name):
+    logging.basicConfig(filename="chat.log", filemode="w", level=logging.INFO, format="%(message)s")
+    try:
+        model = LlamaCpp(model_path=model, n_ctx=ctx_size, n_threads=cores, callback_manager=CallbackManager([StreamingStdOutCallbackHandler()]), verbose=False)
+    except:
+        print("cannot load model")    
+    llm = ConversationChain(llm=model, memory=ConversationBufferMemory(ai_prefix=ai_name, verbose=False))  
+
     print (Fore.YELLOW + "llmon-py")
     user_input = None
     while user_input != "exit":
         user_input = input(Fore.BLUE + "> ")
-        print (Fore.RED)
-        llm.predict(input=user_input)
+        logging.info(f"{user_name}: {user_input}")
+        print (Fore.RED)      
+        try:
+            llm_response = llm.predict(input=user_input)
+            logging.info(f"{ai_name}: {llm_response}")
+        except:
+            print(Fore.YELLOW + "token limit reached")
         print("\n")
-
+              
 if __name__ == "__main__":
-    main("based-30b.ggmlv3.q4_K_M.bin", 2048, 10, "Johnny 5")
+    main("based-30b.ggmlv3.q4_K_M.bin", 2048, 10, "Johnny 5", "User")
