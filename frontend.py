8b7bdfed8b8842866c149c7b49fd5ef0978f0636
3eeps <55860052+3eeps@users.noreply.github.com>
2023-08-10 09:09:54 -0400

    chat logging

--- frontend.py
+++ frontend.py
@@ -1,20 +1,24 @@
 # /codespace/llm-frontend.py
 import logging
-from datetime import date
 from colorama import Fore
-from langchain import LlamaCpp, ConversationChain
-from langchain.memory import ConversationBufferMemory
+from langchain import LlamaCpp, ConversationChain, PromptTemplate
+from langchain.memory import ConversationBufferMemory, ConversationEntityMemory, CombinedMemory
 from langchain.callbacks.manager import CallbackManager
 from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
 
-def main(model=str, ctx_size=int, cores=int, gpu_layers=int, ai_name=str, user_name=str):
-    date_log = date.today()
-    logging.basicConfig(filename=f"{user_name}-{ai_name}-{date_log}.log", filemode="w", level=logging.INFO, format="%(message)s")
+def main(model=str, ctx_size=int, cpu_cores=int, gpu_layers=int, ai_name=str, user_name=str):
+    with open("chat.log") as file:
+        chat_log = file.readlines()
+
+    memory = ConversationBufferMemory(ai_prefix=ai_name)
+    callback_manager = CallbackManager([StreamingStdOutCallbackHandler()])
+
+    logging.basicConfig(filename="chat.log", filemode="w", level=logging.INFO, format="%(message)s")
     try:
-        model = LlamaCpp(model_path=model, n_gpu_layers=gpu_layers, n_ctx=ctx_size, n_threads=cores, callback_manager=CallbackManager([StreamingStdOutCallbackHandler()]), verbose=False)
+        model = LlamaCpp(model_path=model, n_gpu_layers=gpu_layers, n_ctx=ctx_size, n_threads=cpu_cores, callback_manager=callback_manager, verbose=False)
     except:
         print("cannot load model")    
-    llm = ConversationChain(llm=model, memory=ConversationBufferMemory(ai_prefix=ai_name, verbose=False))  
+    llm = ConversationChain(llm=model, memory=memory, verbose=False)  
 
     print (Fore.YELLOW + "llmon-py")
     user_input = str
@@ -28,6 +32,6 @@ def main(model=str, ctx_size=int, cores=int, gpu_layers=int, ai_name=str, user_n
         except:
             print(Fore.YELLOW + "token limit reached")
         print("\n")
-              
+    
 if __name__ == "__main__":
     main("based-30b.ggmlv3.q4_K_M.bin", 2048, 10, 4, "Johnny 5", "User")
