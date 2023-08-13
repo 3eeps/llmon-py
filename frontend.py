13734707c888468a69eccddc5a5aa694aa2d3918
3eeps <55860052+3eeps@users.noreply.github.com>
2023-08-13 10:20:41 -0400

    Update frontend.py

--- frontend.py
+++ frontend.py
@@ -14,24 +14,17 @@ def main(model=str, ctx_size=int, cpu_cores=int, gpu_layers=int, ai_name=str, us
     callback_manager = CallbackManager([StreamingStdOutCallbackHandler()])
 
     logging.basicConfig(filename="chat.log", filemode="w", level=logging.INFO, format="%(message)s")
-    try:
-        model = LlamaCpp(model_path=model, n_gpu_layers=gpu_layers, n_ctx=ctx_size, n_threads=cpu_cores, callback_manager=callback_manager, verbose=False)
-    except:
-        print("cannot load model")    
-    llm = ConversationChain(llm=model, memory=memory, verbose=False)  
+    model = LlamaCpp(model_path=model, n_gpu_layers=gpu_layers, n_ctx=ctx_size, n_threads=cpu_cores, callback_manager=callback_manager, verbose=False)
+    llm = ConversationChain(llm=model, memory=memory, verbose=False)
 
     print (Fore.YELLOW + "llmon-py")
-    user_input = str
-    while user_input != "exit":
+    while True:
         user_input = input(Fore.BLUE + "> ")
         logging.info(f"{user_name}: {user_input}")
         print (Fore.RED)      
-        try:
-            llm_response = llm.predict(input=user_input)
-            logging.info(f"{ai_name}: {llm_response}")
-        except:
-            print(Fore.YELLOW + "token limit reached")
+        llm_response = llm.predict(input=user_input)
+        logging.info(f"{ai_name}: {llm_response}")
         print("\n")
     
 if __name__ == "__main__":
-    main("based-30b.ggmlv3.q4_K_M.bin", 2048, 10, 4, "Johnny 5", "User")
+    main("based-7B.ggmlv3.q4_1.bin", 2048, 10, 35, "Johnny 5", "User")
