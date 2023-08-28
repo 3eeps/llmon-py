b057dbee0cbfaaff4d73e09da7af20663807478b
3eeps <55860052+3eeps@users.noreply.github.com>
2023-08-28 13:47:59 -0400

    Update frontend.py

--- frontend.py
+++ frontend.py
@@ -1,23 +1,47 @@
 # /codespace/llmon.py
-
+import os
 from colorama import Fore
-from langchain import LlamaCpp, ConversationChain
+from langchain import LlamaCpp, ConversationChain, PromptTemplate, LLMChain
 from langchain.memory import ConversationBufferMemory
 from langchain.callbacks.manager import CallbackManager
 from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
 
-def main(model=str, ctx_size=int, batch_size=int, cpu_cores=int, ai_name=str, user_name=str):
-    callback_manager = CallbackManager([StreamingStdOutCallbackHandler()])
-    memory = ConversationBufferMemory(ai_prefix=ai_name)
-    model = LlamaCpp(model_path=model, n_batch=batch_size, n_ctx=ctx_size, n_threads=cpu_cores, verbose=False, callback_manager=callback_manager)
-    llm = ConversationChain(llm=model, memory=memory, verbose=False)
+def load_model():
+    model_path = "./models"
+    model_list = []
+    file_list = os.scandir(model_path)
+    print(Fore.YELLOW + "llmon-py" + Fore.LIGHTBLACK_EX + model_path, "\n")
+    list_num = 0
+    for obj in file_list:
+        list_num = list_num + 1
+        if obj.is_file():
+            print(Fore.GREEN + f"{list_num}.)" + Fore.YELLOW +  f"{obj.name}")
+            model_list.append(obj.name)
+    file_list.close()
 
-    print (Fore.YELLOW + "llmon-py")
+    model_num = int(input(Fore.LIGHTBLACK_EX + ">>> "))
+    model_to_load = model_list[model_num - 1]   
+    return model_to_load
+
+def main(model=str, ctx_size=int, cpu_cores=int, ai_name=str, user_name=str):
+    template = """Your name is {ai_name}, and you are my AI friend, we always have a blast learning and chatting together and speak whats on our mind.
+
+    User: {prompt}
+    ASSISTANT:"""
+    prompt = PromptTemplate(
+    input_variables=["prompt", "ai_name"],
+    template=template)
+
+    callback_manager = CallbackManager([StreamingStdOutCallbackHandler()])
+    llama_model = LlamaCpp(model_path=model, n_ctx=ctx_size, n_batch=1024, n_threads=cpu_cores, verbose=False, callback_manager=callback_manager)
+    llm = LLMChain(llm=llama_model, verbose=False, prompt=prompt)
+
+    print (Fore.YELLOW + "llmon-py " + Fore.LIGHTBLACK_EX + "topped with " + Fore.GREEN + f"{model}")
     while True:
-        user_input = input(Fore.BLUE + f"{user_name} >>> ")
-        print (Fore.RED)      
-        llm.predict(input=user_input)
+        user_prompt = input(Fore.BLUE + f"{user_name} >>> ")
+        print (Fore.RED)       
+        llm.run({"prompt" : user_prompt, "ai_name" : ai_name})
         print("\n")
 
-if __name__ == "__main__":
-    main(model="./models/based-30b.ggmlv3.q4_K_M.bin", ctx_size=2048, batch_size=1024, cpu_cores=12, ai_name="Johnny 5", user_name="fusion")
+if __name__ == "__main__":   
+    main(model=f"./models/{load_model()}", ctx_size=2048, cpu_cores=12, user_name="fusion", ai_name="Johnny 5")
