3c68c094e99926e9c767a0bd095ab8892a87ff16
3eeps <55860052+3eeps@users.noreply.github.com>
2023-11-05 12:37:13 -0500

    Update llmon.py

--- llmon.py
+++ llmon.py
@@ -4,7 +4,8 @@ import os
 from TTS.api import TTS 
 from llama_cpp import Llama
 from pywhispercpp.model import Model
-from nltk.tokenize import sent_tokenize
+#from nltk.tokenize import sent_tokenize
+#import rssnews
 
 import simpleaudio
 import sounddevice 
@@ -13,43 +14,55 @@ from scipy.io.wavfile import write
 class Client():
     def __init__(self):
         self.rec_sample_freq = 44100
-        self.rec_seconds = 10
+        self.rec_seconds = 8
         self.rec_channels = 2
-        self.test_output = './voices/aftertest.wav'
         self.user_file_count = 0
         self.rec_user_voice_file = f'user_chatout-{self.user_file_count}.wav'
-        self.stt_model = Model('./models/stt/ggml-tiny.bin')
+        self.stt_model = Model('./stt/ggml-tiny.bin')
+        self.clone_voice_file = 'redguard0.wav'
+        self.log_file = 'chat_history.log'
 
-        self.chat_template = str
+        self.chat_template = str                                                                                                                                                                                                                            
         self.file_count = 0
         self.new_session = True
         self.model_path = "./models"
         self.model_list = []
-        self.model_count = 0
+        self.model_count = 1
         self.path_list = os.scandir(self.model_path)
 
         self.tts_model = TTS("tts_models/multilingual/multi-dataset/xtts_v1").cuda()
 
+        self.gpu_layers = 22
+        self.model_ctx = 4096
+        self.gpu_device = 0
+        self.chat_format = "chatml"
+        self.verbose_output = False
+
+        self.green = 32
+        self.yellow = 93
+        self.red = 91
+        self.grey  = 90
+        self.white = 37
+
     def log_chat(self, user_message=str, model_message=str):
+        write_type = 'w'
         if self.new_session == False:
             write_type = 'a'
-        else:
-            write_type = 'w'
         self.new_session = False
 
-        messages_to_log = [f'USER: {user_message}', f'model_output{self.file_count}.wav: {model_message}']
-        with open('chat_history.log', f'{write_type}') as output:
-            for line in messages_to_log:
-                output.write(line)
-                output.write('\n')
-        output.close()
+        output_line = [f'USER: {user_message}', f'model_output{self.file_count}.wav: {model_message}']
+        with open(self.log_file, f'{write_type}') as output_file:
+            for message in output_line:
+                output_file.write(message)
+                output_file.write('\n')
+        output_file.close()
 
     def color(self, color_code):
         return f"\33[{color_code}m".format(code=color_code)
 
     def create_chat_wav(self, chat_model_text=str):
         self.file_count = self.file_count + 1
-        self.tts_model.tts_to_file(text=chat_model_text, speaker_wav='./voices/redguard0.wav', file_path=f'./model_output{self.file_count}.wav', language="en")
+        self.tts_model.tts_to_file(text=chat_model_text, speaker_wav=f'./voices/{self.clone_voice_file}', file_path=f'./model_output{self.file_count}.wav', language="en")
 
     def play_wav(self):
         wav_filename = f'./model_output{self.file_count}.wav'
@@ -73,33 +86,32 @@ class Client():
         write(filename=self.rec_user_voice_file, rate=self.rec_sample_freq, data=rec_user_voice)
                                  
     def update_chat_template(self, prompt=str):
+        
+        chatml = f"""<|im_start|>system
+        You are a scientist from the videogame Half-life. You are one of few who were able to escape the Resonance Cascade event that happened at the Black Mesa facility. You love to answer questions about your experiences during the event.<|im_end|>
+        <|im_start|>user
+        {prompt}<|im_end|>
+        <|im_start|>assistant"""
 
-        basic = f"""{prompt}"""
-
-        chatml = f"""system
-        You are an helpful AI assisant. Please take all text from the user prompt, and repeat it back. Thank you!
-        user
-        {prompt}
-        assistant"""
-
-        vicuna = f"""Your name is Johnny 5, and you are an AI assistant. Please feel free to express your thoughts and opinions on topics we discuss. Thank you!
+        vicuna = f"""You are a scientist from the videogame Half-life. You are one of few who were able to escape the Resonance Cascade event that happened at the Black Mesa facility. You love to answer questions about your experiences during the event.
 
         User: {prompt}
         ASSISTANT:"""
-        template_type = vicuna
+
+        template_type = chatml
         return template_type
 
     def select_models(self):
         os.system("cls")
-        print(self.color(93) + "llmon-py" + self.color(90) + self.model_path, "\n")
+        print(self.color(self.yellow) + "llmon-py" + self.color(self.grey) + self.model_path, "\n")
         for model_file in self.path_list:
-            self.model_count = self.model_count + 1
             if model_file.is_file():
-                print(self.color(32) + f"{self.model_count}.) " + self.color(93) + f"{model_file.name}")
+                print(self.color(self.green) + f"{self.model_count}.) " + self.color(self.yellow) + f"{model_file.name}")
                 self.model_list.append(model_file.name)
+            self.model_count = self.model_count + 1
         self.path_list.close()
 
-        print(self.color(90))
+        print(self.color(self.grey))
         model_int = int(input('#>> '))
         chat_model_filename = self.model_list[model_int - 1]
         os.system("cls")
@@ -108,23 +120,31 @@ class Client():
     def start(self):
         running_client = True
         chat_model_loaded = self.select_models()
-        print (self.color(93) + "llmon-py " + self.color(90) + "using " + self.color(37) + f"{chat_model_loaded}")
-        chat_model = Llama(model_path=f'./models/{chat_model_loaded}', main_gpu=0, n_gpu_layers=22, n_ctx=2048, verbose=False, chat_format="vicuna")
+        print (self.color(self.yellow) + "llmon-py " + self.color(self.grey) + "using " + self.color(self.white) + f"{chat_model_loaded}")
+        chat_model = Llama(
+            model_path=f'./models/{chat_model_loaded}', 
+            main_gpu = self.gpu_device, 
+            n_gpu_layers = self.gpu_layers, 
+            n_ctx = self.model_ctx, 
+            verbose = self.verbose_output, 
+            chat_format = self.chat_format)
       
         while running_client:
             #feed = rssnews.parse_url(urls=['https://www.cbc.ca/webfeed/rss/rss-canada'])
-            user_prompt = input(self.color(34) + "user>>> ")
-            if user_prompt == '':
-                self.record_user()  
-                text = self.voice_to_text()
-                prompt = self.update_chat_template(prompt=text)
-            else:               
-                prompt = self.update_chat_template(user_prompt)
+            user_text_prompt = input(self.color(self.yellow) + "user>>> ")
+            if user_text_prompt == '':
+                print("Recording for {self.rec_seconds}:")
+                self.record_user()
+                user_voice_prompt = self.voice_to_text()
+                prompt = self.update_chat_template(user_voice_prompt)
+            else:  
+                #read_test = """Kelly got the drip coffee machine going, then he pulled on a pair of swim trunks and headed topside. He hadn't forgotten to set the anchor light, he was gratified to see. The sky had cleared off, and the air was cool after the thunderstorms of the previous night. He went forward and was surprised to see that one of his anchors had dragged somewhat. Kelly reproached himself for that, even though nothing had actually gone wrong. The water was a flat, oily calm and the breeze gentle. The pink-orange glow of first light decorated the tree-spotted coastline to the east. All in all, it seemed as fine a morning as he could remember. Then he remembered that what had changed had nothing at all to do with the weather."""
+                prompt = self.update_chat_template(user_text_prompt)
 
             chat_model_output = chat_model(prompt=prompt) 
-            self.log_chat(user_message=user_prompt, model_message=chat_model_output['choices'][0]['text'])
-            print(self.color(31))
-            self.create_chat_wav(chat_model_output['choices'][0]['text'])            
+            self.log_chat(user_message=user_text_prompt, model_message=chat_model_output['choices'][0]['text'])
+            print(self.color(self.green))
+            self.create_chat_wav(chat_model_output['choices'][0]['text'])
             self.play_wav()
 
 if __name__ == "__main__":
