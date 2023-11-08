4479e4b6f216b3ffaccd8721fac334a8c3371479
3eeps <55860052+3eeps@users.noreply.github.com>
2023-11-08 13:32:21 -0500

    Update llmon.py

--- llmon.py
+++ llmon.py
@@ -1,6 +1,9 @@
 # ./codespace/llmon.py
-import os
 
+import os
+import warnings
+
+import torch
 from TTS.api import TTS 
 from llama_cpp import Llama
 from pywhispercpp.model import Model
@@ -11,6 +14,13 @@ import readchar
 import simpleaudio
 import sounddevice 
 from scipy.io.wavfile import write
+
+import GPUtil
+from threading import Thread
+import time
+
+# sorry, getting too many warnings from all this ai jazz
+warnings.filterwarnings("ignore")
 
 def display_logo():
     # when life gives you lemons, you paint that shit gold
@@ -33,12 +43,28 @@ def display_logo():
 ⠀⠀⢸⣿⣿⣿⣿⣿⣿⣿⣿⠿⠶⣾⣿⣉⣡⣤⣿⠿⠛⠁⠀⠀⠀⠀⠀⠀⠀⠀
 ⠀⠀⢸⣿⣿⣿⣿⡿⠿⠿⠿⠶⠾⠛⠛⠛⠉⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
 ⠀⠀⠈⠉⠉⠉""")
-    
+
+class Monitor(Thread):
+    def __init__(self, delay):
+        super(Monitor, self).__init__()
+        self.stopped = False
+        self.delay = delay # Time between calls to GPUtil
+        self.start()
+
+    def run(self):
+        while not self.stopped:
+            GPUtil.showUtilization()
+            time.sleep(self.delay)
+            os.system("cls")
+
+    def stop(self):
+        self.stopped = True
+
 class Client():
     def __init__(self):
         self.gpu_layers = 20
         self.model_ctx = 4096
-        self.gpu_device = 0
+        self.device = "cuda" if torch.cuda.is_available() else "cpu"
         self.verbose_output = False
 
         self.rec_sample_freq = 44100
@@ -47,9 +73,7 @@ class Client():
         self.user_file_count = 0
         self.model_file_count = 0
         self.model_list_count = 1
-        self.rec_user_voice_file = f'./user_output-{self.user_file_count}.wav'
-        self.rec_model_output_file = f'./model_output-{self.model_file_count}.wav'
-        self.clone_voice_file = './voices/artbell.mp3'
+        self.clone_voice_file = './voices/artbell.wav'
         self.log_file = './chat_history.log'
 
         self.new_session = True
@@ -64,7 +88,7 @@ class Client():
         self.grey  = 90
         self.white = 37
 
-        self.text_ts_model = TTS("tts_models/multilingual/multi-dataset/xtts_v2").to(device=self.gpu_device)
+        self.text_ts_model = TTS("tts_models/multilingual/multi-dataset/xtts_v2").to(device=self.device)
         self.speech_tt_model = Model('./stt/ggml-tiny.bin')
 
     def log_chat(self, user_message=str, model_message=str):
@@ -85,7 +109,7 @@ class Client():
 
     def create_chat_wav(self, chat_model_text=str):
         self.model_file_count = self.model_file_count + 1
-        self.text_ts_model.tts_to_file(text=chat_model_text, speaker_wav=self.clone_voice_file, file_path=self.rec_model_output_file, language="en")
+        self.text_ts_model.tts_to_file(text=chat_model_text, speaker_wav=self.clone_voice_file, file_path=f'./model_output-{self.model_file_count}.wav', language="en")
 
     def play_wav(self):
         wav_filename = f'./model_output-{self.model_file_count}.wav'
@@ -95,7 +119,7 @@ class Client():
 
     def voice_to_text(self):
         text_data = []
-        user_voice_data = self.speech_tt_model.transcribe(f'./{self.rec_user_voice_file}')
+        user_voice_data = self.speech_tt_model.transcribe(f'user_output-{self.user_file_count}.wav')
         for voice in user_voice_data:        
             text_data.append(voice.text)
         combined_text = ' '.join(text_data)
@@ -106,7 +130,7 @@ class Client():
         self.user_file_count = self.user_file_count + 1
         rec_user_voice = sounddevice.rec(int(self.rec_seconds * self.rec_sample_freq), samplerate=self.rec_sample_freq, channels=self.rec_channels)
         sounddevice.wait()
-        write(filename=self.rec_user_voice_file, rate=self.rec_sample_freq, data=rec_user_voice)
+        write(filename=f'user_output-{self.user_file_count}.wav', rate=self.rec_sample_freq, data=rec_user_voice)
                                  
     def update_chat_template(self, prompt=str):
 
@@ -166,10 +190,12 @@ class Client():
             else:  
                 prompt = self.update_chat_template(user_text_prompt)
 
+            #monitor = Monitor(5)
             chat_model_output = chat_model(prompt=prompt) 
             self.log_chat(user_message=user_text_prompt, model_message=chat_model_output['choices'][0]['text'])
             print(self.color(self.green))
             self.create_chat_wav(chat_model_output['choices'][0]['text'])
+            #monitor.stop()
             self.play_wav()
 
 if __name__ == "__main__":
