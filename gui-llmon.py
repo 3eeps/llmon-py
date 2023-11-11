dc87be00acf3d0a6852737039a2d299e43c81039
3eeps <55860052+3eeps@users.noreply.github.com>
2023-11-11 14:42:00 -0500

    Update gui-llmon.py

--- gui-llmon.py
+++ gui-llmon.py
@@ -15,7 +15,7 @@ from scipy.io.wavfile import write
 warnings.filterwarnings("ignore")
 st.title("llmon-py")
 
-char_name = "Art"
+char_name = "Scientist"
 rec_seconds = 8
 
 if "messages" not in st.session_state:
@@ -25,89 +25,96 @@ if 'text_ts_model' not in st.session_state:
     st.session_state.text_ts_model = text_ts_model = TTS("tts_models/multilingual/multi-dataset/xtts_v2").to('cuda')
 
 if 'chat_model' not in st.session_state:
-    st.session_state.chat_model = Llama(model_path="models/airoboros-mistral2.2-7b.Q5_K_M.gguf",  n_gpu_layers = 32, n_ctx = 4096, verbose = False)
+    st.session_state.chat_model = Llama(model_path="models/airoboros-mistral2.2-7b.Q6_K.gguf",  n_gpu_layers = -1, n_ctx = 4096, verbose = False)
 
 if 'speech_tt_model' not in st.session_state:
     st.session_state.speech_tt_model = Model(models_dir="models")
 
-def display_logo():
-        # when life gives you lemons, you paint that shit gold
-        # thanks https://emojicombos.com/lemon-ascii-art, for the lemon
-        os.system("cls")
-        color_logo = f"\33[{93}m".format(code=93)
-        print(f"""{color_logo}
+def llmon():
+    # when life gives you lemons, you paint that shit gold
+    # thanks https://emojicombos.com/lemon-ascii-art, for the lemon
+    os.system("cls")
+    color_logo = f"\33[{93}m".format(code=93)
+    print(f"""{color_logo}
                         llmon-py
-    ⠀⠀⠀⠀⠀⢀⣀⣠⣤⣴⣶⡶⢿⣿⣿⣿⠿⠿⠿⠿⠟⠛⢋⣁⣤⡴⠂⣠⡆⠀
-    ⠀⠀⠀⠀⠈⠙⠻⢿⣿⣿⣿⣶⣤⣤⣤⣤⣤⣴⣶⣶⣿⣿⣿⡿⠋⣠⣾⣿⠁⠀
-    ⠀⠀⠀⠀⠀⢀⣴⣤⣄⡉⠛⠻⠿⠿⣿⣿⣿⣿⡿⠿⠟⠋⣁⣤⣾⣿⣿⣿⠀⠀
-    ⠀⠀⠀⠀⣠⣾⣿⣿⣿⣿⣿⣶⣶⣤⣤⣤⣤⣤⣤⣶⣾⣿⣿⣿⣿⣿⣿⣿⡇⠀
-    ⠀⠀⠀⣰⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀
-    ⠀⠀⢰⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠁⠀
-    ⠀⢀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠇⢸⡟⢸⡟⠀⠀
-    ⠀⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⢿⣷⡿⢿⡿⠁⠀⠀
-    ⠀⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡟⢁⣴⠟⢀⣾⠃⠀⠀⠀
-    ⠀⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠛⣉⣿⠿⣿⣶⡟⠁⠀⠀⠀⠀
-    ⠀⠀⢿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠿⠛⣿⣏⣸⡿⢿⣯⣠⣴⠿⠋⠀⠀⠀⠀⠀⠀
-    ⠀⠀⢸⣿⣿⣿⣿⣿⣿⣿⣿⠿⠶⣾⣿⣉⣡⣤⣿⠿⠛⠁⠀⠀⠀⠀⠀⠀⠀⠀
-    ⠀⠀⢸⣿⣿⣿⣿⡿⠿⠿⠿⠶⠾⠛⠛⠛⠉⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-    ⠀⠀⠈⠉⠉⠉""")
+⠀⠀⠀⠀⠀⢀⣀⣠⣤⣴⣶⡶⢿⣿⣿⣿⠿⠿⠿⠿⠟⠛⢋⣁⣤⡴⠂⣠⡆⠀
+⠀⠀⠀⠀⠈⠙⠻⢿⣿⣿⣿⣶⣤⣤⣤⣤⣤⣴⣶⣶⣿⣿⣿⡿⠋⣠⣾⣿⠁⠀
+⠀⠀⠀⠀⠀⢀⣴⣤⣄⡉⠛⠻⠿⠿⣿⣿⣿⣿⡿⠿⠟⠋⣁⣤⣾⣿⣿⣿⠀⠀
+⠀⠀⠀⠀⣠⣾⣿⣿⣿⣿⣿⣶⣶⣤⣤⣤⣤⣤⣤⣶⣾⣿⣿⣿⣿⣿⣿⣿⡇⠀
+⠀⠀⠀⣰⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀
+⠀⠀⢰⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠁⠀
+⠀⢀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠇⢸⡟⢸⡟⠀⠀
+⠀⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⢿⣷⡿⢿⡿⠁⠀⠀
+⠀⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡟⢁⣴⠟⢀⣾⠃⠀⠀⠀
+⠀⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠛⣉⣿⠿⣿⣶⡟⠁⠀⠀⠀⠀
+⠀⠀⢿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠿⠛⣿⣏⣸⡿⢿⣯⣠⣴⠿⠋⠀⠀⠀⠀⠀⠀
+⠀⠀⢸⣿⣿⣿⣿⣿⣿⣿⣿⠿⠶⣾⣿⣉⣡⣤⣿⠿⠛⠁⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⢸⣿⣿⣿⣿⡿⠿⠿⠿⠶⠾⠛⠛⠛⠉⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠈⠉⠉⠉""")
 
 def update_chat_template(prompt=str):
-        instruction = f"""### Instruction: 
-        none
+    instruction = f"""### Instruction: 
+    none
 
-        GUEST: {prompt}
-        ### Response:"""
+    GUEST: {prompt}
+    ### Response:"""
         
-        user_assist_art = f"""USER: You are Art Bell, the radio host from the late-night talk show, Coast to Coast AM. Your guest tonight claims to be a theoretical physicist with a remarkable story. He claims to have worked at the top-secret Black Mesa research facility, where he witnessed an unimaginable disaster.
+    user_assist_art = f"""USER: You are Art Bell, the radio host from the late-night talk show, Coast to Coast AM. Your guest tonight claims to be a theoretical physicist with a remarkable story. He claims to have worked at the top-secret Black Mesa research facility, where he witnessed an unimaginable disaster.
 
-        GUEST: {prompt}
-        ASSISTANT:"""
+    GUEST: {prompt}
+    ASSISTANT:"""
 
-        user_assist_hlsci = f"""USER: You are a former scientist from the Black Mesa reseach facility. You escaped the resonance cascade event and made it to the surface. You are here to share you stories when questioned.
+    user_assist_hlsci = f"""USER: You are a former scientist from the Black Mesa reseach facility. You escaped the resonance cascade event and made it to the surface. You are here to share you stories when questioned.
 
-        GUEST: {prompt}
-        ASSISTANT:"""
+    GUEST: {prompt}
+    ASSISTANT:"""
 
-        vicuna = f"""none
+    vicuna = f"""none
 
-        User: {prompt}
-        ASSISTANT:"""
+    User: {prompt}
+    ASSISTANT:"""
 
-        template_type = user_assist_hlsci
-        return template_type
+    template_type = user_assist_hlsci
+    return template_type
 
 def create_chat_wav(chat_model_text=str):
-        character_wav = "voices/artbell.wav"
-        st.session_state.text_ts_model.tts_to_file(text=chat_model_text, speaker_wav=character_wav, file_path='model_output.wav', language="en")
+    character_wav = "voices/hlscientist2.wav"
+    add_text_color = f"\33[{32}m".format(code=32)
+    print(add_text_color)
+    st.session_state.text_ts_model.tts_to_file(text=chat_model_text, speaker_wav=character_wav, file_path='model_output.wav', language="en")
 
 def play_wav():
-        wav_filename = 'model_output.wav'
-        wav_object = simpleaudio.WaveObject.from_wave_file(wav_filename)
-        play_audio = wav_object.play()
-        play_audio.wait_done()
+    wav_filename = 'model_output.wav'
+    wav_object = simpleaudio.WaveObject.from_wave_file(wav_filename)
+    play_audio = wav_object.play()
+    play_audio.wait_done()
 
 def voice_to_text():
-        text_data = []
-        user_voice_data = st.session_state.speech_tt_model.transcribe('user_output.wav')
-        for voice in user_voice_data:        
-            text_data.append(voice.text)
-        combined_text = ' '.join(text_data)
-        print(combined_text)
-        return combined_text
-
-def record_user():
     rec_user_voice = sounddevice.rec(int(rec_seconds * 44100), samplerate=44100, channels=2)
     sounddevice.wait()
     write(filename='user_output.wav', rate=44100, data=rec_user_voice)
 
-display_logo()
+    text_data = []
+    user_voice_data = st.session_state.speech_tt_model.transcribe('user_output.wav')
+    for voice in user_voice_data:        
+        text_data.append(voice.text)
+    combined_text = ' '.join(text_data)
+    print(combined_text)
+    return combined_text
+
+llmon()
 for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
 
 if user_prompt := st.chat_input(f"Send a message to {char_name}"):
-    prompt = update_chat_template(user_prompt)
+    if user_prompt == 'voice':
+        user_voice_text = voice_to_text()
+        user_prompt = user_voice_text
+        prompt = update_chat_template(user_voice_text)
+    else:
+        prompt = update_chat_template(user_prompt)
+    
     with st.chat_message("User"):
         st.markdown(user_prompt)
     st.session_state.messages.append({"role": "user", "content": user_prompt})
