5a1398eb987bfeb7c0ffadd5c82979f8fa14d166
3eeps <55860052+3eeps@users.noreply.github.com>
2023-11-11 13:28:48 -0500

    Update gui-llmon.py

--- gui-llmon.py
+++ gui-llmon.py
@@ -16,7 +16,7 @@ warnings.filterwarnings("ignore")
 st.title("llmon-py")
 
 char_name = "Art"
-#rec_seconds = 8
+rec_seconds = 8
 
 if "messages" not in st.session_state:
     st.session_state.messages = []
@@ -25,7 +25,7 @@ if 'text_ts_model' not in st.session_state:
     st.session_state.text_ts_model = text_ts_model = TTS("tts_models/multilingual/multi-dataset/xtts_v2").to('cuda')
 
 if 'chat_model' not in st.session_state:
-    st.session_state.chat_model = Llama(model_path="models/airoboros-mistral2.2-7b.Q5_K_M.gguf",  n_gpu_layers = -1, n_ctx = 4096, verbose = False)
+    st.session_state.chat_model = Llama(model_path="models/airoboros-mistral2.2-7b.Q5_K_M.gguf",  n_gpu_layers = 32, n_ctx = 4096, verbose = False)
 
 if 'speech_tt_model' not in st.session_state:
     st.session_state.speech_tt_model = Model(models_dir="models")
@@ -54,37 +54,42 @@ def display_logo():
 
 def update_chat_template(prompt=str):
         instruction = f"""### Instruction: 
-        empty
+        none
 
         GUEST: {prompt}
         ### Response:"""
         
-        user_assist = f"""USER: You are Art Bell, the radio host from the late-night talk show, Coast to Coast AM. Your guest tonight claims to be a theoretical physicist with a remarkable story. He claims to have worked at the top-secret Black Mesa research facility, where he witnessed an unimaginable disaster.
+        user_assist_art = f"""USER: You are Art Bell, the radio host from the late-night talk show, Coast to Coast AM. Your guest tonight claims to be a theoretical physicist with a remarkable story. He claims to have worked at the top-secret Black Mesa research facility, where he witnessed an unimaginable disaster.
 
         GUEST: {prompt}
         ASSISTANT:"""
 
-        vicuna = f""" empty
+        user_assist_hlsci = f"""USER: You are a former scientist from the Black Mesa reseach facility. You escaped the resonance cascade event and made it to the surface. You are here to share you stories when questioned.
+
+        GUEST: {prompt}
+        ASSISTANT:"""
+
+        vicuna = f"""none
 
         User: {prompt}
         ASSISTANT:"""
 
-        template_type = user_assist
+        template_type = user_assist_hlsci
         return template_type
 
 def create_chat_wav(chat_model_text=str):
         character_wav = "voices/artbell.wav"
-        st.session_state.text_ts_model.tts_to_file(text=chat_model_text, speaker_wav=character_wav, file_path=f'model_output.wav', language="en")
+        st.session_state.text_ts_model.tts_to_file(text=chat_model_text, speaker_wav=character_wav, file_path='model_output.wav', language="en")
 
 def play_wav():
-        wav_filename = f'model_output.wav'
+        wav_filename = 'model_output.wav'
         wav_object = simpleaudio.WaveObject.from_wave_file(wav_filename)
         play_audio = wav_object.play()
         play_audio.wait_done()
 
 def voice_to_text():
         text_data = []
-        user_voice_data = st.session_state.speech_tt_model.transcribe(f'user_output.wav')
+        user_voice_data = st.session_state.speech_tt_model.transcribe('user_output.wav')
         for voice in user_voice_data:        
             text_data.append(voice.text)
         combined_text = ' '.join(text_data)
@@ -94,7 +99,7 @@ def voice_to_text():
 def record_user():
     rec_user_voice = sounddevice.rec(int(rec_seconds * 44100), samplerate=44100, channels=2)
     sounddevice.wait()
-    write(filename=f'user_output.wav', rate=44100, data=rec_user_voice)
+    write(filename='user_output.wav', rate=44100, data=rec_user_voice)
 
 display_logo()
 for message in st.session_state.messages:
