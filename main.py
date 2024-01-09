2e2391d80cb403bc14a4c28aadacf0f3b0024306
Chad Canning <55860052+3eeps@users.noreply.github.com>
2024-01-09 16:09:28 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,12 +2,11 @@
 
 import streamlit as st
 from streamlit_extras.app_logo import add_logo
-
 import GPUtil as GPU
 import os
+import torch
 import keyboard
 import random
-import time
 
 keyboard.unhook_all()
 st.set_page_config(page_title="llmon-py", page_icon="🍋", layout="wide", initial_sidebar_state="auto")
@@ -27,52 +26,66 @@ def scan_dir(directory):
 
 GPUs = GPU.getGPUs()
 gpu = GPUs[0]
-model_box_data = scan_dir('models')
-voice_box_data = scan_dir('voices')
 
-def popup_note(message=str):
-        st.toast(message)
-        time.sleep(2.0)
+model_box_data = scan_dir('./models')
+voice_box_data = scan_dir('./voices')
+boop_box_data = scan_dir('./ui-tones')
+lora_list = scan_dir("./loras")
 
-check_vram = float("{0:.0f}".format(gpu.memoryUsed)) / float("{0:.0f}".format(gpu.memoryTotal))
-if check_vram > 0.85:
-    popup_note(message='😭 vram limit is being reached')
+chat_templates = ['vicuna_based', 'deepseek', 'user_assist_rick', 'user_assist_duke', 'ajibawa_python', 'user_assist_art', 'user_assist_kyle', 'user_assist_hlsci', 'custom']
+model_names = ['Assistant', 'Dr. Rosenburg', 'Duke', 'Rick', 'Cortana', 'Kyle Katarn', 'Art Bell', 'Bot', 'AI', 'Model', 'Johnny 5', 'Codebot']
+
 st.progress(float("{0:.0f}".format(gpu.memoryFree)) / float("{0:.0f}".format(gpu.memoryTotal)), "vram {0:.0f}/{1:.0f}mb".format(gpu.memoryUsed, gpu.memoryTotal))
+st.session_state.clear_vram = st.toggle('clear vram', value=False)
+tab1, tab2, tab3, tab4 = st.tabs(["🔊audio", "💭chat model", "🔗advanced", '👀 sdxl'])
 
-tab1, tab2, tab3, tab4 = st.tabs(["🔊audio", "💭chat model", "🔗advanced", '👀 sdxl turbo'])
 with tab1:
-    st.header("audio")          
+    st.header("audio")
+    st.session_state.boop_select = st.selectbox('message beep', boop_box_data)
+    st.audio(f"./ui-tones/{st.session_state.boop_select}")          
     st.session_state.enable_microphone = st.toggle('enable microphone', value=False)
     st.session_state.microphone_hotkey = st.selectbox('microphone hotkey', ['ctrl'])
     st.session_state.enable_voice = st.toggle('enable tts model', value=False)
     st.session_state.enable_code_voice = st.toggle('enable coding mode', value=False)
     st.session_state.user_audio_length = st.slider("microphone rec time(sec)", 2, 25, 8)
     st.session_state.audio_cuda_or_cpu = st.selectbox('audio inference to', ["cuda", "cpu"])
-    st.session_state.voice_select = st.selectbox('voice file', [voice_box_data[0], voice_box_data[1], voice_box_data[2], voice_box_data[3], voice_box_data[4], voice_box_data[5], voice_box_data[6]])
+    st.session_state.voice_select = st.selectbox('voice file', voice_box_data)
+    st.audio(f"./voices/{st.session_state.voice_select}")
 
 with tab2:
     st.header("chat model")
-    st.session_state.model_select = st.selectbox('model file', [model_box_data[0], model_box_data[1], model_box_data[2], model_box_data[3]])  
-    st.session_state.template_select = st.selectbox('chat template', ['vicuna_based', 'user_assist_rick', 'user_assist_duke', 'ajibawa_python', 'user_assist_art', 'user_assist_kyle', 'user_assist_hlsci'])
-    st.session_state.char_name = st.selectbox('model name', ['Assistant', 'Dr. Rosenburg', 'Duke', 'Rick', 'Cortana', 'Kyle Katarn', 'Art Bell', 'Bot', 'AI', 'Model'])
+    st.session_state.model_select = st.selectbox('model file', model_box_data)
+    st.session_state.template_select = st.selectbox('chat template', chat_templates)
+    st.session_state.char_name = st.selectbox('model name', model_names)
     st.session_state.model_language = st.selectbox('tts language', ["en", 'es'])
 
 with tab3:
     st.header("advanced")
+    #st.session_state.enable_summarizer = st.toggle('summarize text', value=False)
     st.session_state.enable_popups = st.toggle('enable system popups', value=True)
     st.session_state.console_warnings = st.selectbox('hide console warnings', ['ignore', 'default'])
     st.session_state.verbose_chat = st.toggle('enable verbose console', value=True)
-    st.session_state.max_context_prompt = int(st.selectbox('max token gen', ['1536', '256', '512', '1024', '2048', '4096', '8096', '16384', '32768']))
+    st.session_state.max_context_prompt = int(st.selectbox('max token gen', ['1024', '256', '512', '1536', '2048', '4096', '8096', '16384', '32768']))
     st.session_state.max_context = int(st.selectbox('max context size', ['4096', '8096', '16384', '32768']))
     st.session_state.torch_audio_cores = st.slider('torch audio cores', 2, 64, 8)
     st.session_state.gpu_layer_count = st.slider('gpu layers', -1, 128, -1)
-    st.session_state.cpu_core_count = st.slider('cpu cores', 1, 128, 12)
-    st.session_state.cpu_batch_count = st.slider('cpu batch cores', 1, 128, 12)
+    st.session_state.cpu_core_count = st.slider('cpu cores', 1, 128, 8)
+    st.session_state.cpu_batch_count = st.slider('cpu batch cores', 1, 128, 8)
     st.session_state.batch_size= st.slider('batch size', 0, 1024, 256)
     st.session_state.stream_chunk_size = st.slider('stream chunk size', 20, 200, 40)
-    st.session_state.chunk_buffer = st.slider('chunk buffers', 0, 2, 1)
+    st.session_state.chunk_pre_buffer = st.slider('chunk buffers', 0, 2, 2)
     
 with tab4:
     st.header("sdxl turbo")
-    st.session_state.enable_sdxl = st.toggle('enable sdxl turbo', value=False)
-    st.session_state.enable_cpu_only = st.toggle('run on cpu', value=False)
+    st.session_state.enable_sdxl_turbo = st.toggle('enable sdxl turbo', value=False)
+    st.session_state.enable_cpu_only = st.toggle('run turbo on cpu', value=False)
+    st.header("sdxl 1.0")
+    st.session_state.enable_sdxl = st.toggle('enable sdxl 1.0', value=False)
+    st.session_state.lora_to_load = st.session_state.lora_to_load = st.selectbox('lora to load', lora_list)
+
+st.write('session state details')
+st.json(st.session_state, expanded=False)
+
+if st.session_state.clear_vram:
+    del st.session_state.chat_model
+    torch.cuda.empty_cache()
