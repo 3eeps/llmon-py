f91b111fe4b2bc2f73d9aac13f5fff6e60f33539
Chad Canning <55860052+3eeps@users.noreply.github.com>
2024-01-28 14:13:11 -0500

    Update home.py

--- home.py
+++ home.py
@@ -1,28 +1,15 @@
-# ./codespace/main.py
-
+# ./codespace/home.py
 import streamlit as st
 from streamlit_extras.app_logo import add_logo
-import os
-import torch
 
 st.set_page_config(page_title="llmon-py", page_icon="🍋", layout="wide", initial_sidebar_state="auto")
 st.title('llmon-py')
 add_logo("./llmonpy/pie.png", height=130)
 
-def scan_dir(directory):
-    directory_list = []
-    count = 0
-    for file in os.scandir(f'{directory}'):
-        if file.is_file():
-            directory_list.append(file.name)
-            count += count
-    return directory_list
+from llmonpy import llmonaid
 
-model_box_data = scan_dir('./models')
-voice_box_data = scan_dir('./voices')
-lora_list = scan_dir("./loras")
-
-# pull from text files?
+model_box_data = llmonaid.scan_dir('./models')
+voice_box_data = llmonaid.scan_dir('./voices')
 chat_templates = ['vicuna_default', 'deepseek', 'user_assist_rick', 'user_assist_duke', 'ajibawa_python', 'user_assist_art', 'user_assist_kyle', 'user_assist_hlsci']
 default_context_list = [128, 256, 512, 1024, 2048, 4096, 8096, 16384, 32768]
 default_max_context_list = [2048, 4096, 8096, 16384, 32768]
@@ -30,7 +17,6 @@ default_max_context_list = [2048, 4096, 8096, 16384, 32768]
 model_box_dict = {}
 voice_box_dict = {}
 chat_template_dict = {}
-lora_list_dict = {}
 default_context_dict = {}
 default_max_context_dict = {}
 
@@ -38,65 +24,25 @@ counter = 0
 for i in model_box_data:
     model_box_dict[counter] = model_box_data[counter]
     counter += 1
-
 counter = 0
 for i in voice_box_data:
     voice_box_dict[counter] = voice_box_data[counter]
     counter += 1
-
 counter = 0
 for i in chat_templates:
     chat_template_dict[counter] = chat_templates[counter]
     counter += 1
-
-counter = 0
-for i in lora_list:
-    lora_list_dict[counter] = lora_list[counter]
-    counter += 1
-
 counter = 0
 for i in default_context_list:
     default_context_dict[counter] = default_context_list[counter]
     counter += 1
-
 counter = 0
 for i in default_max_context_list:
     default_max_context_dict[counter] = default_max_context_list[counter]
     counter += 1
 
-st.session_state.clear_vram = st.toggle('clear vram', value=False)
-
-default_settings_state = {'enable_message_beep': True,
-                          'enable_microphone': False,
-                          'enable_voice': False,
-                          'tts_cpu': False,
-                          'enable_code_voice': False,
-                          'user_audio_length': 8,
-                          'voice_select': voice_box_data[0],
-                          'model_select': model_box_data[0],
-                          'template_select': chat_templates[0],
-                          'enable_popups': True,
-                          'verbose_chat': True,
-                          'max_context_prompt': 1024,
-                          'max_context': 4096,
-                          'torch_audio_cores': 8,
-                          'gpu_layer_count': -1,
-                          'cpu_core_count': 8,
-                          'cpu_batch_count': 8,
-                          'batch_size': 256,
-                          'stream_chunk_size': 35,
-                          'chunk_pre_buffer': 3,
-                          'enable_sdxl_turbo': False,
-                          'img2img_on': False,
-                          'turbo_cpu': False,
-                          'enable_sdxl': False,
-                          'sdxl_cpu': False,
-                          'ocr_device': False}
-
-for key, value in default_settings_state.items():
-    if key not in st.session_state:
-        st.session_state[key] = value
-        
+llmonaid.init_default_state(model_box_data, voice_box_data, chat_templates)
+st.session_state.clear_vram = st.toggle('clear vram', value=False)     
 tab1, tab2, tab3, tab4 = st.tabs(["🔊audio", "💭chat model", "🔗advanced", '👀 image gen/ocr'])
 with tab1:
     st.header("audio")
@@ -139,7 +85,6 @@ with tab2:
 
 with tab3:
     st.header("advanced")
-    st.session_state['enable_popups'] = st.toggle('enable system popups', value=st.session_state['enable_popups'])
     st.session_state['verbose_chat'] = st.toggle('enable verbose console', value=st.session_state['verbose_chat'])
 
     set_context_index = 0
@@ -173,32 +118,7 @@ with tab4:
     st.header("moondream1 (ocr)")
     st.session_state['ocr_device'] = st.toggle('run on cpu', value=st.session_state['ocr_device'], key='ocr_cpu')
 
-st.json(st.session_state, expanded=False)
-
-# pickle state, dont include the following vars and then unpickle? may drop them from the session state better
+llmonaid.clear_buffers()
 if st.session_state.clear_vram:
-    try:
-        del st.session_state.vision_encoder
-    except:
-        pass
-
-    try:
-        del st.session_state.text_model
-    except:
-        pass
-
-    try:
-        del st.session_state.model
-    except:
-        pass
-
-    try:
-        del st.session_state.config
-    except:
-        pass
-
-    try:
-        del st.session_state['chat_model']
-    except:
-        pass
-    torch.cuda.empty_cache()
+    llmonaid.clear_vram()
+st.json(st.session_state, expanded=False)
