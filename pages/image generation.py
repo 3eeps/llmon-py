663afe50e3296209459148f857468edef5988f74
Chad Canning <55860052+3eeps@users.noreply.github.com>
2024-01-28 09:02:19 -0500

    Update image generation.py

--- pages/image generation.py
+++ pages/image generation.py
@@ -3,112 +3,49 @@
 import streamlit as st
 from streamlit_extras.app_logo import add_logo
 from st_keyup import st_keyup
-import os
 
 st.set_page_config(page_title="image generation", page_icon="🍋", layout="wide", initial_sidebar_state="auto")
-st.title("llmon-py - image generation")
-add_logo("./llmon_art/pie.png", height=130)
+st.title("image generation")
+add_logo("./llmonpy/pie.png", height=130)
 
-import time
-import torch
-from diffusers import DiffusionPipeline, AutoPipelineForText2Image, AutoPipelineForImage2Image
-from diffusers.utils import load_image
-from datetime import datetime
+from llmonpy import generation
 
-def scan_dir(directory):
-    directory_list = []
-    count = 0
-    for file in os.scandir(f'{directory}'):
-        if file.is_file():
-            directory_list.append(file.name)
-            count += count
-    return directory_list
-
-def popup_note(message=str):
-    if st.session_state['enable_popups']:
-        st.toast(message)
-
-lora_path = "./loras"
-image_list = scan_dir('./images')
-device = 'cuda'
-
-default_step = 0
-if st.session_state['enable_sdxl']:
-    default_step = 50
-if st.session_state['enable_sdxl_turbo']:
-    default_step = 1
-if st.session_state['img2img_on']:
-    default_step = 1
+image_list = generation.scan_dir('./images')
+lora_list = generation.scan_dir("./loras")
 
 with st.sidebar:
-    st.text(f"lora: {st.session_state['lora_to_load']}")
     notepad = st.text_area(label='notepad', label_visibility='collapsed')
+    if st.session_state['enable_sdxl']:
+        lora_selected = st.selectbox('lora for sdxl', lora_list)
     if st.session_state['img2img_on']:
         image_selected = st.selectbox('image for img2img', image_list)
-    steps = st.slider('steps', 1, 50, default_step)
-    iter_count = st.slider('number of images', 1, 32, 1)
-    gen_buffer = st.slider('sdxl turbo gen buffer', 50, 5000, 1000)
-
-if 'img2img_pipe' not in st.session_state and st.session_state['img2img_on']:
-    if st.session_state['turbo_cpu']:
-        device = 'cpu'   
-    st.session_state['img2img_pipe'] = AutoPipelineForImage2Image.from_pretrained("stabilityai/sdxl-turbo", torch_dtype=torch.float16, variant="fp16").to(device)
-
-if 'image_pipe_sdxl' not in st.session_state and st.session_state['enable_sdxl']:
-    popup_note(message='👊 hiting up sdxl 1.O...')
-    if st.session_state['sdxl_cpu']:
-        device = 'cpu'
-    st.session_state.image_pipe_sdxl = DiffusionPipeline.from_pretrained(pretrained_model_name_or_path="stabilityai/stable-diffusion-xl-base-1.0", torch_dtype=torch.float16).to(device)
-    st.session_state.image_pipe_sdxl.load_lora_weights(pretrained_model_name_or_path_or_dict=lora_path, weight_name=st.session_state['lora_to_load'])
-    st.session_state.image_pipe_sdxl.enable_vae_slicing()
-    st.session_state.image_pipe_sdxl.enable_model_cpu_offload()
+    if st.session_state['enable_sdxl'] or st.session_state['img2img_on']:
+        iter_count = st.slider('number of images', 1, 32, 1)
 
 if 'image_pipe_turbo' not in st.session_state and st.session_state['enable_sdxl_turbo']:
-    popup_note(message='👊 hiting up sdxl turbo...')
-    st.session_state.image_pipe_turbo = AutoPipelineForText2Image.from_pretrained("stabilityai/sdxl-turbo", torch_dtype=torch.float16, variant="fp16")
-    if st.session_state['turbo_cpu']:
-        device = 'cpu'
-    st.session_state.image_pipe_turbo.to(device)
+    generation.popup_note(message='👊 hiting up sdxl turbo...')
+    generation.load_sdxl_turbo()
 
-def create_image_turbo():
-    image_prompt = st_keyup(label='real time(ish) image generation using sdxl turbo', debounce=gen_buffer)
-    image = st.session_state.image_pipe_turbo(prompt=image_prompt, num_inference_steps=steps, guidance_scale=0.0).images[0]
-    image.save('image_turbo.png')
-    time.sleep(0.2)
-    st.image('image_turbo.png')
+if 'image_pipe_sdxl' not in st.session_state and st.session_state['enable_sdxl']:
+    generation.popup_note(message='👊 hiting up sdxl 1.O...')
+    generation.load_sdxl()
 
-def create_image_sdxl(image_prompt=str, image_count=int):
-    
-    image_list = []
-    while image_count:     
-        image = st.session_state.image_pipe_sdxl(prompt=image_prompt, num_inference_steps=50).images[0]
-        output_file_name = datetime.now().strftime("%d-%m-%Y-%H-%M-%S")
-        image.save(f"{output_file_name}-{image_count}.png")
-        image_list.append(f"{output_file_name}-{image_count}.png")
-        st.image(image_list)
-        image_count = image_count - 1
-        if len(image_list) > 32:
-            image_list = []
+if 'img2img_pipe' not in st.session_state and st.session_state['img2img_on']:
+    generation.popup_note(message='👊 hiting up sdxl turbo img2img...')
+    generation.load_turbo_img2img()
 
-def create_img2img(prompt=str, image_count=int):
-    while image_count:
-        init_image = load_image(f"./images/{image_selected}").resize((512, 512))
-        image = st.session_state['img2img_pipe'](prompt, image=init_image, num_inference_steps=steps, strength=0.5, guidance_scale=0.0).images[0]
-        output_file_name = datetime.now().strftime("img2img-%d-%m-%Y-%H-%M-%S")
-        image.save(f"img2img-{output_file_name}.png")
-        image_count = image_count - 1
-
-if st.session_state['enable_sdxl_turbo']: 
-    create_image_turbo()
+if st.session_state['enable_sdxl_turbo']:
+    turbo_prompt = st_keyup(label='real time(ish) image generation using sdxl turbo', debounce=1000) 
+    generation.create_image_turbo(prompt=turbo_prompt)
 
 if st.session_state['enable_sdxl']:
     sdxl_prompt = st.text_input(label='image generation with sdxl 1.0')
     if st.button('submit'):
-        create_image_sdxl(image_prompt=sdxl_prompt, image_count=iter_count)
-
+        st.image(generation.create_image_sdxl(prompt=sdxl_prompt, iterations=iter_count, lora_name=lora_selected))
+        
 if st.session_state['img2img_on']:
     img2img_prompt = st.text_input(label='image to image generation with sdxl turbo')
-    img2img_button = st.button("submit", key='img2img')
+    send_img2img_prompt = st.button("submit", key='img2img')
     st.image(f"./images/{image_selected}")
-    if img2img_button:
-        create_img2img(prompt=img2img_prompt, image_count=iter_count)
+    if send_img2img_prompt:
+        st.image(generation.create_image_img2img(from_image=image_selected, prompt=img2img_prompt, iterations=iter_count))
