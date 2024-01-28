70b91690cd999b8fdbf3b6ba388355d2859f5d63
Chad Canning <55860052+3eeps@users.noreply.github.com>
2024-01-28 14:14:45 -0500

    Update image reconition.py

--- pages/image reconition.py
+++ pages/image reconition.py
@@ -1,5 +1,4 @@
 # ./codespace/pages/image reconition.py
-
 import streamlit as st
 from streamlit_extras.app_logo import add_logo
 
@@ -8,12 +7,10 @@ st.title("image reconition")
 add_logo("./llmonpy/pie.png", height=130)
 
 import re
-import time
-from llmonpy import reconition
+from llmonpy import reconition, llmonaid
 
 with st.sidebar:
     notepad = st.text_area(label='notepad', label_visibility='collapsed')
-
     uploaded_file = st.file_uploader(label="Choose a image file")
     if uploaded_file is not None:
         st.session_state.bytes_data = uploaded_file.getvalue()
@@ -21,10 +18,10 @@ with st.sidebar:
         st.session_state.buffer = ""
 
 if 'vision_encoder' not in st.session_state:
-    reconition.popup_note(enable=st.session_state['enable_popups'])
+    llmonaid.popup_note(message='👀 looking at you moondream...')
     reconition.load_vision_encoder(enable_cpu=st.session_state['ocr_device'])
 
-moondream_prompt = st.text_input(label="image reconition with :orange[moondream1]", value='can you describe this image?')
+moondream_prompt = st.text_input(label="image reconition with :orange[moondream]")
 send_prompt = st.button("submit")
 
 if uploaded_file is not None:
@@ -33,13 +30,8 @@ else:
     st.image(image=f"./llmonpy/pie.png", caption=re.sub("<$", "", re.sub("END$", "", st.session_state.buffer)))
 
 if send_prompt:
-    start_time = time.time()
     response = reconition.generate_response(image_data=st.session_state.bytes_data, prompt=moondream_prompt)
-
     st.session_state.buffer = ""
     for word in response:
         st.session_state.buffer += word
-
-    with st.sidebar:
-        st.write(f"elapsed time: {int(time.time()-start_time)} secs")
     st.rerun()
