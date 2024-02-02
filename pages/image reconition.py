b7df7c59944835e34bd7f53914996c4bf77a4a2b
Chad Canning <55860052+3eeps@users.noreply.github.com>
2024-02-02 12:42:16 -0500

    Update image reconition.py

--- pages/image reconition.py
+++ pages/image reconition.py
@@ -1,37 +1,27 @@
-# ./codespace/pages/image reconition.py
+# ./codespace/llmon/reconition.py
 import streamlit as st
-from streamlit_extras.app_logo import add_logo
+import os
+from moondream import VisionEncoder, TextModel
+from PIL import Image
+from threading import Thread
+from transformers import TextIteratorStreamer
 
-st.set_page_config(page_title="image reconition", page_icon="🍋", layout="wide", initial_sidebar_state="auto")
-st.title("image reconition")
-add_logo("./llmonpy/pie.png", height=130)
+image_ocr_model_path = "c:\codespace\moondream\moondream1"
 
-import re
-from llmonpy import reconition, llmonaid
+def load_vision_encoder(enable_cpu=bool):
+    st.session_state['vision_encoder'] = VisionEncoder(image_ocr_model_path, run_on=enable_cpu)
+    st.session_state['text_model'] = TextModel(image_ocr_model_path, run_on=enable_cpu)
 
-with st.sidebar:
-    notepad = st.text_area(label='notepad', label_visibility='collapsed')
-    uploaded_file = st.file_uploader(label="Choose a image file")
-    if uploaded_file is not None:
-        st.session_state.bytes_data = uploaded_file.getvalue()
-    else:
-        st.session_state.buffer = ""
+def generate_response(image_data, prompt=str):
+    filename = "ocr_upload_image.png"
+    with open(filename, 'wb') as file:
+        file.write(image_data)
+    image = Image.open("ocr_upload_image.png")
 
-if 'vision_encoder' not in st.session_state:
-    llmonaid.popup_note(message='👀 looking at you moondream...')
-    reconition.load_vision_encoder(enable_cpu=st.session_state['ocr_device'])
-
-moondream_prompt = st.text_input(label="image reconition with :orange[moondream]")
-send_prompt = st.button("submit")
-
-if uploaded_file is not None:
-    st.image(image=st.session_state.bytes_data, caption=re.sub("<$", "", re.sub("END$", "", st.session_state.buffer)))
-else:
-    st.image(image=f"./llmonpy/pie.png", caption=re.sub("<$", "", re.sub("END$", "", st.session_state.buffer)))
-
-if send_prompt:
-    response = reconition.generate_response(image_data=st.session_state.bytes_data, prompt=moondream_prompt)
-    st.session_state.buffer = ""
-    for word in response:
-        st.session_state.buffer += word
-    st.rerun()
+    image_embeds = st.session_state['vision_encoder'](image)
+    streamer = TextIteratorStreamer(st.session_state['text_model'].tokenizer, skip_special_tokens=True)
+    generation_kwargs = dict(image_embeds=image_embeds, question=prompt, streamer=streamer)
+    thread = Thread(target=st.session_state['text_model'].answer_question, kwargs=generation_kwargs)
+    thread.start()
+    os.remove("ocr_upload_image.png")
+    return streamer
