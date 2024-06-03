4631105354765a6c49cd7f5234da60296b140e9e
Chad Canning <55860052+3eeps@users.noreply.github.com>
2024-06-03 06:17:19 -0400

    Add files via upload

--- llmon.py
+++ llmon.py
@@ -12,6 +12,13 @@ from PIL import Image
 from transformers import AutoModelForCausalLM, AutoTokenizer
 
 def model_inference(prompt=""):
+    if st.session_state.function_calling:
+        st.session_state['model_tempurature'] = 0.85
+        st.session_state['model_min_p'] = 0.06
+        st.session_state['model_top_p'] = 0.0
+        st.session_state['model_top_k'] = 0
+        st.session_state['repeat_penalty'] = 1.1
+
     model_output = st.session_state["chat_model"](prompt=prompt,
                                                         repeat_penalty=float(st.session_state['repeat_penalty']),
                                                         max_tokens=st.session_state['max_context'], 
@@ -27,25 +34,24 @@ def clear_vram():
         del st.session_state[model_in_vram]
 
 def init_state():
-    default_settings_state = {'user_audio_length': 8,
-                            'max_context': 8192,
-                            'gpu_layer_count': -1,
-                            'cpu_core_count': 8,
-                            'cpu_batch_count': 8,
-                            'batch_size': 256,
-                            'model_output_tokens': 0,
-                            'model_temperature': 0.85,
-                            'model_top_p': 0.0,
-                            'model_top_k': 0,
-                            'model_min_p': 0.06,
-                            'repeat_penalty': 1.1,
-                            'message_list': [],
-                            'init_app': True,
-                            'model_select': 'llama-3-8b-instruct.gguf'}
+    default_settings_state = {
+        'max_context': 8192,
+        'gpu_layer_count': -1,
+        'cpu_core_count': 8,
+        'cpu_batch_count': 8,
+        'batch_size': 256,
+        'model_temperature': 0.95,
+        'model_top_p': 0.90,
+        'model_top_k': 120,
+        'model_min_p': 0.0,
+        'repeat_penalty': 1.5,
+        'message_list': [],
+        'init_app': True,
+        'model_select': 'Meta-Llama-3-8B-Instruct.Q6_K.gguf'}
     
     st.session_state.function_calling = False
     st.session_state.function_results = ""
-    st.session_state.sys_prompt = ""
+    st.session_state.custom_template = ""
     st.session_state.video_link = None
     st.session_state.first_watch = False
     st.session_state.bytes_data = None
@@ -61,8 +67,8 @@ def sidebar():
     if st.button(label=':orange[new chat]'):
         st.session_state['message_list'] = []
         st.session_state.messages = []
-        st.session_state['model_output_tokens'] = 0
         st.session_state.function_calling = False
+        st.session_state.custom_template = ""
 
     uploaded_file = st.file_uploader(label='file uploader', label_visibility='collapsed', type=['png', 'jpeg'])
     if uploaded_file:
@@ -70,15 +76,20 @@ def sidebar():
         with open("ocr_upload_image.png", 'wb') as file:
             file.write(st.session_state.bytes_data)
 
-    st.session_state.function_calling = st.toggle(':orange[enable function calling]', value=st.session_state.function_calling)
+    st.session_state.function_calling = st.toggle(':orange[enable function calling] :green[(beta)]', value=st.session_state.function_calling)
     st.caption(body="custom model template")
-    st.session_state.sys_prompt = st.text_area(label='custom prompt', value="", label_visibility='collapsed')
+    st.session_state.custom_template = st.text_area(label='custom prompt', value="", label_visibility='collapsed', disabled=st.session_state.function_calling)
     st.caption(body="model parameters")
-    st.session_state['model_temperature'] = st.text_input(label=':orange[temperature]', value=st.session_state['model_temperature'])
-    st.session_state['model_top_p'] = st.text_input(label=':orange[top p]', value=st.session_state['model_top_p'] )
-    st.session_state['model_top_k'] = st.text_input(label=':orange[top k]', value=st.session_state['model_top_k'])
-    st.session_state['model_min_p'] = st.text_input(label=':orange[min p]', value=st.session_state['model_min_p'])
-    st.session_state['repeat_penalty'] = st.text_input(label=':orange[repeat_penalty]', value=st.session_state['repeat_penalty'])
+    temp_help = """determinines whether the output is more random and creative or more predictable. :green[a higher temperature will result in lower probability], i.e more creative outputs."""
+    top_p_help = """controls the diversity of the generated text by only considering tokens with the highest probability mass. :green[top_p = 0.1: only tokens within the top 10% probability are considered. 0.9: considers tokens within the top 90% probability]."""
+    top_k_help = """limits the model's output to the top-k most probable tokens at each step. This can help reduce incoherent or nonsensical output by restricting the model's vocabulary. :green[a top-K of 1 means the next selected token is the most probable among all tokens in the model's vocabulary]."""
+    min_p_help = """different from top k or top p, sets a minimum percentage requirement to consider tokens relative to the largest token probability. :green[for example, min p = 0.1 is equivalent to only considering tokens at least 1/10th the top token probability]."""
+    rep_help = """helps the model generate more diverse content instead of repeating previous phrases. Repetition is prevented by applying a high penalty to phrases or words that tend to be repeated. :green[a higher penalty generally results in more diverse outputs, whilst a lower value might lead to more repetition]."""
+    st.session_state['model_temperature'] = st.text_input(label=':orange[temperature]', value=st.session_state['model_temperature'], disabled=st.session_state.function_calling, help=temp_help)
+    st.session_state['model_top_p'] = st.text_input(label=':orange[top p]', value=st.session_state['model_top_p'], disabled=st.session_state.function_calling, help=top_p_help)
+    st.session_state['model_top_k'] = st.text_input(label=':orange[top k]', value=st.session_state['model_top_k'], disabled=st.session_state.function_calling, help=top_k_help)
+    st.session_state['model_min_p'] = st.text_input(label=':orange[min p]', value=st.session_state['model_min_p'], disabled=st.session_state.function_calling, help=min_p_help)
+    st.session_state['repeat_penalty'] = st.text_input(label=':orange[repetition penalty]', value=st.session_state['repeat_penalty'], disabled=st.session_state.function_calling, help=rep_help)
     if st.button(":violet[shutdown]", help='shut down app on server side'):
         clear_vram()
         try:
@@ -91,33 +102,29 @@ def sidebar():
 
 class ChatTemplate:
     def chat_template(prompt="", function_result=""):
-        system_message = f"""You are an AI assistant who acts more as the users friend. Provide simple and relaxed responses to any inquiries the user has.
-        The user is roughly 35 years old and is well versed in most topics. Reply more as a close friend then an AI assistant. Our conversation history: {st.session_state['message_list']}"""
+        system_message = f"""You are an helpful AI assistant, answer any request the user may have. Share postive or negative considerations when appropriate, but keep them concise. Conversation history: {st.session_state['message_list']}"""
         template = f"""<|begin_of_text|<|start_header_id|>system<|end_header_id|>{system_message}<|eot_id|><|start_header_id|>user<|end_header_id|>{prompt}<|eot_id|><|start_header_id|>assistant<|end_header_id|>"""
 
         if st.session_state.function_calling:
-            system_message = f"""As an AI assistant with function calling support, you are provided with the following functions: 
-            Functions: {json.dumps(st.session_state.functions)}
-            Chat history: {st.session_state['message_list']}
-            When the user asks a question that can be answered with one of the above functions, only output the function filled with the appropriate data required as a python dictionary.
-            Only output functions found in the json file provided to you. Do not describe the function, 'present it', or wrap it in markdown. Do not say: Here is the function call:"""
-            system_message_plus_example = """Example: User: 'play brother ali take me home' You: '{'function_name': 'video_player', 'parameters': {'youtube_query': 'brother ali take me home'}}'"""
+            system_message = f"""As an AI assistant with function calling support, you are provided with the following functions: {json.dumps(st.session_state.functions)}
+            When the user asks a question that can be answered with a function, do not describe the function or wrap the function in "||", only output the function filled with the appropriate data required as a Python dictionary."""
+            system_message_plus_example = """Example message: 'Hello there!' Your reply: '{'function_name': 'user_chat', 'parameters': {'user_message': 'Hello there!'}}'"""
             template = f"""<|begin_of_text|<|start_header_id|>system<|end_header_id|>{system_message + system_message_plus_example}<|eot_id|><|start_header_id|>user<|end_header_id|>{prompt}<|eot_id|><|start_header_id|>assistant<|end_header_id|>"""
 
             normal_reply = False
             if function_result == "func_reply":
                 normal_reply = True
-                system_message = f"""You are an AI assistant who acts more as the users friend. Provide simple and relaxed responses to any inquiries the user has.
-                The user is roughly 35 years old and is well versed in most topics. Reply more as a close friend then an AI assistant. Our conversation history: {st.session_state['message_list']}"""
+                system_message = f"""You are an helpful AI assistant, answer any request the user may have. Share postive or negative considerations when appropriate. Conversation history: {st.session_state['message_list']}"""
                 template = f"""<|begin_of_text|<|start_header_id|>system<|end_header_id|>{system_message}<|eot_id|><|start_header_id|>user<|end_header_id|>{prompt}<|eot_id|><|start_header_id|>assistant<|end_header_id|>"""
 
             if len(function_result) > 1 and normal_reply == False:
                 system_message = f"""Using this data that is up to date, reply to the user using it: {function_result}. The question the user asked was:"""
                 template = f"""<|begin_of_text|<|start_header_id|>system<|end_header_id|>{system_message}<|eot_id|><|start_header_id|>user<|end_header_id|>{prompt}<|eot_id|><|start_header_id|>assistant<|end_header_id|>"""
 
-        if len(st.session_state.sys_prompt) > 1:
-            system_message = f"""{st.session_state.sys_prompt} Chat history: {st.session_state['message_list']}"""
+        if len(st.session_state.custom_template) > 1:
+            system_message = f"""{st.session_state.custom_template} Chat history: {st.session_state['message_list']}"""
             template = f"""<|begin_of_text|<|start_header_id|>system<|end_header_id|>{system_message}<|eot_id|><|start_header_id|>user<|end_header_id|>{prompt}<|eot_id|><|start_header_id|>assistant<|end_header_id|>"""                 
+
         return template
 
 class Audio:
@@ -156,8 +163,8 @@ class SDXLTurbo:
 class Moondream:
     def init():
         model_id = "vikhyatk/moondream2"
-        revision = "2024-05-08"
-        st.session_state['moondream'] = AutoModelForCausalLM.from_pretrained(model_id, trust_remote_code=True, revision=revision).to('cpu')
+        revision = "2024-05-20"
+        st.session_state['moondream'] = AutoModelForCausalLM.from_pretrained(model_id, trust_remote_code=True, revision=revision)
         st.session_state.tokenizer = AutoTokenizer.from_pretrained(model_id, revision=revision)
 
     def generate_response(prompt=str):
--- main.py
+++ main.py
@@ -13,13 +13,12 @@ if "messages" not in st.session_state:
     st.session_state.messages = []
 
 if "chat_model" not in st.session_state:
-    st.session_state["chat_model"] = Llama(model_path=f"./llama-3-8b-instruct.gguf",
-                                            n_batch=st.session_state['batch_size'],
-                                            n_threads=st.session_state['cpu_core_count'],
-                                            n_threads_batch=st.session_state['cpu_batch_count'],
-                                            n_gpu_layers=int(st.session_state['gpu_layer_count']),
-                                            n_ctx=st.session_state['max_context'],
-                                            verbose=True)
+    st.session_state["chat_model"] = Llama(model_path=f"./{st.session_state['model_select']}",
+        n_batch=st.session_state['batch_size'],
+        n_threads=st.session_state['cpu_core_count'],
+        n_threads_batch=st.session_state['cpu_batch_count'],
+        n_gpu_layers=int(st.session_state['gpu_layer_count']),
+        n_ctx=st.session_state['max_context'], verbose=True)
 
 if "moondream" not in st.session_state:
     llmon.Moondream.init()
@@ -35,8 +34,8 @@ with st.sidebar:
     llmon.sidebar()
 
 for message in st.session_state.messages:
-    message = st.chat_message(name=message['role'])
-    message.write(message["content"])
+    with st.chat_message(name=message['role']):
+        st.write(message["content"])
 
 if user_text_input:= st.chat_input(placeholder=''):
     user_input = user_text_input
@@ -67,29 +66,31 @@ if user_text_input:= st.chat_input(placeholder=''):
             print(f"""{model_output_dict}\n{func_value_dict}\n{output_function}\n{value_name}""")
             
             if output_function == "user_chat":
-                st.session_state.function_results = "func_reply"
+                st.session_state.function_results = 'func_reply'
+                text_output = True
 
             if output_function == "describe_image":
                 st.session_state.function_results = llmon.Moondream.generate_response(prompt=value_name[0])
 
             if output_function == "create_image":
                 llmon.SDXLTurbo.generate_image(prompt=value_name[0])
-                no_text_output = True
+                text_output = False
             
             if output_function == "video_player":
                 st.session_state.video_link = llmon.Functions.find_youtube_link(user_query=value_name[0])
                 st.session_state.first_watch = False
-                link_text_output = True
+                display_link = True
                 st.session_state.messages.append({"role": "assistant", "content": value_name[0]})
                 st.session_state['message_list'].append(f"You: {value_name[0]}")
 
             final_function_template = llmon.ChatTemplate.chat_template(prompt=user_input, function_result=st.session_state.function_results)
-            if no_text_output == False:
+            if text_output:
                 model_output_text = llmon.model_inference(prompt=final_function_template)
-            if no_text_output: 
+            if text_output == False: 
                 model_output_text = ""
-            if link_text_output:
-                model_output_text = llmon.model_inference(prompt=value_name[0])
+            if display_link:
+                model_output_text = value_name[0]
+
     except: pass
 
     with st.chat_message(name="assistant"):
